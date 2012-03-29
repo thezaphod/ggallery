247bad8d754614e5abd79478c6ce4dd08d8d711a
Igor Stelmashenko <igor@intermobile.ca>
2012-03-29 06:17:17 -0700

    view album added

---
+++ grails-app/controllers/ggallery/AlbumController.groovy
@@ -0,0 +1,73 @@
+package ggallery
+
+class AlbumController {
+
+    // injected
+    def aws
+
+    def index() { }
+
+    def create() {
+
+        String albumId = System.currentTimeMillis()
+
+
+        def email = new Email(from: params.from, to: params.to, subject: params.subject, text: params.email, url: "${getSiteUrl(request)}/album/show?id=" )
+        email.save(failOnError: true, flush:true)
+
+        def album = new Album(title: params.title, photos: new HashSet(), email: email, url: "${getSiteUrl(request)}/album/show?id=")
+
+        // create album
+        session.images.each() { filePath ->
+
+
+            def pubUrl = aws.s3().on("ggallery").publicUrlFor(1.hour, filePath)
+            def f = new Photo(filePath: filePath, caption: filePath, url: pubUrl )
+            f.save(failOnError: true)
+            album.photos.add(f)
+        }
+
+        album.save(failOnError: true)
+
+        String albumUrl = "${getSiteUrl(request)}/album/show?id=" + album.id
+
+        email.url = albumUrl
+        email.save(failOnError: true)
+
+        album.url = albumUrl
+        album.save(failOnError: true)
+
+        email.url = albumUrl
+        email.save(failOnError: true)
+
+        session.album = album
+        redirect(action: "show")
+
+        print email.url
+        print album.id
+    }
+
+    def show() {
+
+        def album
+
+        if (params.id != null) {
+            album = Album.findById(params.id)
+        } else {
+            album = session.album
+        }
+
+       [album: album]
+    }
+
+
+    def static getSiteUrl(request) {
+
+        javax.servlet.http.HttpServletRequest.metaClass.getSiteUrl = {
+
+            return (delegate.scheme + "://" + delegate.serverName + ":" + delegate.serverPort + delegate.getContextPath())
+
+        }
+        return request.getSiteUrl()
+    }
+}
---
+++ grails-app/views/album/show.gsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: igor
+  Date: 12-03-29
+  Time: 4:58 AM
+  To change this template use File | Settings | File Templates.
+--%>
+
+<%@ page contentType="text/html;charset=UTF-8" %>
+<html>
+<head>
+  <title></title>
+</head>
+<body>
+
+</body>
+</html>
